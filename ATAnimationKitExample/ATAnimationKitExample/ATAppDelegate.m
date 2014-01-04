9d0b0458cc0c89e44f1ee046d813947ce94ed8b0
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATAnimationKit][Example] Add linkage to ATBase and ATAnimationKit, add logger

--- ATAnimationKitExample/ATAnimationKitExample/ATAppDelegate.m
+++ ATAnimationKitExample/ATAnimationKitExample/ATAppDelegate.m
@@ -2,10 +2,16 @@
 
 #import "ATAppDelegate.h"
 
+#import <ATBase/DDLog.h>
+#import <ATBase/DDASLLogger.h>
+#import <ATBase/DDTTYLogger.h>
+
 @implementation ATAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
 }
 
 @end
