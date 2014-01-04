1257ad6e91dbc0ae16f755d09e452adbaf2f5e4b
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATBase[ Add -[NSArray subarrayWithoutFirstObject] helper method

---
+++ ATBase/ATBase/NSArray+Helpers.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2013 Andrew Toulouse. All rights reserved.
+
+@interface NSArray (Helpers)
+
+- (NSArray *)subarrayWithoutFirstObject;
+
+@end
---
+++ ATBase/ATBase/NSArray+Helpers.m
@@ -0,0 +1,17 @@
+//  Copyright (c) 2013 Andrew Toulouse. All rights reserved.
+
+#import "NSArray+Helpers.h"
+
+@implementation NSArray (Helpers)
+
+- (NSArray *)subarrayWithoutFirstObject {
+    if (!self.count) {
+        return self;
+    } else if (self.count == 1) {
+        return @[];
+    } else {
+        return [self subarrayWithRange:NSMakeRange(1, self.count - 1)];
+    }
+}
+
+@end
