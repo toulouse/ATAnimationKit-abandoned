16580187e9ede9fad842d2d64dbc964568267117
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATAnimationKit] Add some geometry helpers (except  rotation, because that stuff is *hard*)

---
+++ ATAnimationKit/ATAnimationKit/ATGeometry.c
@@ -0,0 +1,66 @@
+//  Copyright (c) 2013 Andrew Toulouse. All rights reserved.
+
+#include "ATGeometry.h"
+
+#include <math.h>
+
+#pragma mark - Translation (Affine)
+
+CGFloat ATGetAffineTranslationX(CGAffineTransform transform) {
+    return transform.tx;
+}
+
+CGFloat ATGetAffineTranslationY(CGAffineTransform transform) {
+    return transform.ty;
+}
+
+#pragma mark - Scale (Affine)
+
+CGFloat ATGetAffineScaleX(CGAffineTransform transform) {
+    return transform.a;
+}
+CGFloat ATGetAffineScaleY(CGAffineTransform transform) {
+    return transform.d;
+}
+
+#pragma mark - Rotation (Affine)
+
+CGFloat ATGetAffineRotation(CGAffineTransform transform) {
+    return FLT_MAX;
+}
+
+#pragma mark - Translation (3D)
+
+CGFloat ATGet3DTranslationX(CATransform3D transform) {
+    return transform.m41;
+}
+CGFloat ATGet3DTranslationY(CATransform3D transform) {
+    return transform.m42;
+}
+CGFloat ATGet3DTranslationZ(CATransform3D transform) {
+    return transform.m43;
+}
+
+#pragma mark - Scale (3D)
+
+CGFloat ATGet3DScaleX(CATransform3D transform) {
+    return transform.m11;
+}
+CGFloat ATGet3DScaleY(CATransform3D transform) {
+    return transform.m22;
+}
+CGFloat ATGet3DScaleZ(CATransform3D transform) {
+    return transform.m33;
+}
+
+#pragma mark - Rotation (3D)
+
+CGFloat ATGet3DRotationX(CATransform3D transform) {
+    return FLT_MAX;
+}
+CGFloat ATGet3DRotationY(CATransform3D transform) {
+    return FLT_MAX;
+}
+CGFloat ATGet3DRotationZ(CATransform3D transform) {
+    return FLT_MAX;
+}
---
+++ ATAnimationKit/ATAnimationKit/ATGeometry.h
@@ -0,0 +1,44 @@
+//  Copyright (c) 2013 Andrew Toulouse. All rights reserved.
+
+#ifndef ATGEOMETRY_H_
+#define ATGEOMETRY_H_
+
+#include <ATBase/ATBaseDefines.h>
+
+#include <CoreGraphics/CGAffineTransform.h>
+
+#include <QuartzCore/CATransform3D.h>
+
+#pragma mark - Translation (Affine)
+
+AT_EXTERN CGFloat ATGetAffineTranslationX(CGAffineTransform transform);
+AT_EXTERN CGFloat ATGetAffineTranslationY(CGAffineTransform transform);
+
+#pragma mark - Scale (Affine)
+
+AT_EXTERN CGFloat ATGetAffineScaleX(CGAffineTransform transform);
+AT_EXTERN CGFloat ATGetAffineScaleY(CGAffineTransform transform);
+
+#pragma mark - Rotation (Affine)
+
+AT_EXTERN CGFloat ATGetAffineRotation(CGAffineTransform transform);
+
+#pragma mark - Translation (3D)
+
+AT_EXTERN CGFloat ATGet3DTranslationX(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DTranslationY(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DTranslationZ(CATransform3D transform);
+
+#pragma mark - Scale (3D)
+
+AT_EXTERN CGFloat ATGet3DScaleX(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DScaleY(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DScaleZ(CATransform3D transform);
+
+#pragma mark - Rotation (3D)
+
+AT_EXTERN CGFloat ATGet3DRotationX(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DRotationY(CATransform3D transform);
+AT_EXTERN CGFloat ATGet3DRotationZ(CATransform3D transform);
+
+#endif /* ATGEOMETRY_H_ */
