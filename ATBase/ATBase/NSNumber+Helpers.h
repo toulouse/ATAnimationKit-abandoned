64d701ec6a8b560145671dc1e99ab80730ce7f50
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATBase] Add  NSNumber helpers

---
+++ ATBase/ATBase/NSNumber+Helpers.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Andrew Toulouse. All rights reserved.
+
+@interface NSNumber (Helpers)
+
++ (NSNumber *)valueWithCGFloat:(CGFloat)flt;
+- (CGFloat)CGFloatValue;
+
+@end
---
+++ ATBase/ATBase/NSNumber+Helpers.m
@@ -0,0 +1,25 @@
+//  Copyright (c) 2014 Andrew Toulouse. All rights reserved.
+
+#import "NSNumber+Helpers.h"
+
+@implementation NSNumber (Helpers)
+
++ (NSValue *)valueWithCGFloat:(CGFloat)flt
+{
+#if CGFLOAT_IS_DOUBLE
+    return [NSNumber numberWithDouble:flt];
+#else
+    return [NSNumber numberWithFloat:flt];
+#endif
+}
+
+- (CGFloat)CGFloatValue
+{
+#if CGFLOAT_IS_DOUBLE
+    return [self doubleValue];
+#else
+    return [self floatValue];
+#endif
+}
+
+@end
