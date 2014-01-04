7da14fbd6b10b89bbe84507a5313615887d95135
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATBase] Add some convenience asserts

--- ATBase/ATBase/ATAssert.h
+++ ATBase/ATBase/ATAssert.h
@@ -2,14 +2,18 @@
 
 #import "ATBaseDefines.h"
 
+// TODO: ATCAssert
+
 #define ATAssert(condition, ...) [ATAssert _internalAssertWithCondition:(BOOL)(condition) \
                                                          prettyFunction:__PRETTY_FUNCTION__ \
                                                                    file: __FILE__ \
                                                                    line: __LINE__ \
                                                                  format:__VA_ARGS__]
 #define ATFailAssert(...) ATAssert(NO, ##__VA_ARGS__)
+#define ATAssertMainThread() ATAssert([NSThread isMainThread], @"Thread affinity error: called from non-main thread")
+#define ATAssertNotMainThread() ATAssert(![NSThread isMainThread], @"Thread affinity error: called from main thread")
 
-#define AT_IMPLEMENTED_BY_SUBCLASS ATFailAssert(@"Must be implemented by subclass!")
+#define AT_IMPLEMENTED_BY_SUBCLASS ATFailAssert(@"Must be implemented by subclass")
 
 @interface ATAssert : NSObject
 
