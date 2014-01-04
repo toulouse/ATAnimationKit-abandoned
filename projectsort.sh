9fafc2d909d5f509c85dc27859310ffaf8d8a53d
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    Convenience script for sorting project files (for easier merging)

---
+++ projectsort.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./sort-Xcode-project-file.pl ATAnimationKit/ATAnimationKit.xcodeproj/project.pbxproj ATBase/ATBase.xcodeproj/project.pbxproj ATAnimationKitExample/ATAnimationKitExample.xcodeproj/project.pbxproj
