dd9d5ff933ba2ebc9d29c5fa3ac40a5070fb49bc
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATBase] Prettify assertions

--- ATBase/ATBase/ATAssert.m
+++ ATBase/ATBase/ATAssert.m
@@ -7,23 +7,27 @@
 @implementation ATAssert
 
 + (void)_internalAssertWithCondition:(BOOL)condition
-                      prettyFunction:(const char *)prettyFunc
+                      prettyFunction:(const char *)prettyFunction
                                 file:(const char *)file
                                 line:(int)line
                               format:(NSString *)format, ...
 {
-    if (format) {
-        va_list args;
-        va_start(args, format);
-
-        NSString *logMessage = [[NSString alloc] initWithFormat:format arguments:args];
-        NSLog(@"Assertion in %s:%d:\n%@", file, line, logMessage);
-
-        if (!condition) {
-            abort();
+    if (!condition) {
+        NSString *logMessage = nil;
+        if (format) {
+            va_list args;
+            va_start(args, format);
+            logMessage = [[NSString alloc] initWithFormat:format arguments:args];
+            va_end(args);
         }
 
-        va_end(args);
+        if (logMessage) {
+            ATLogError(@"Assertion at %s:%d: %@", prettyFunction, line, logMessage);
+        } else {
+            ATLogError(@"Assertion at %s:%d", prettyFunction, line);
+        }
+
+        abort();
     }
 }
 
