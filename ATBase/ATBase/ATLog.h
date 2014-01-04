0f6343d98e7bad56d6b9ed6fb1b1549d52a39bdb
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    Add Workspace, ATBase framework

---
+++ ATBase/ATBase/ATLog.h
@@ -0,0 +1,17 @@
+//  Copyright (c) 2013 Andrew Toulouse. All rights reserved.
+
+#import "DDLog.h"
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+#define ATLogError    DDLogError
+#define ATLogWarn     DDLogWarn
+#define ATLogInfo     DDLogInfo
+#define ATLogDebug    DDLogDebug
+#define ATLogVerbose  DDLogVerbose
+
+#define ATLogCError   DDLogCError
+#define ATLogCWarn    DDLogCWarn
+#define ATLogCInfo    DDLogCInfo
+#define ATLogCDebug   DDLogCDebug
+#define ATLogCVerbose DDLogCVerbose
