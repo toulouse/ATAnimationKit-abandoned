1a95f297719a1c13cde172849404c0eb5abffd51
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 00:00:00 -0600

    [ATAnimationKit] Add framework header imports

--- ATAnimationKit/ATAnimationKit/ATAnimationKit.h
+++ ATAnimationKit/ATAnimationKit/ATAnimationKit.h
@@ -1,1 +1,15 @@
 //  Copyright (c) 2014 Andrew Toulouse. All rights reserved.
+
+#ifndef ATANIMATIONKIT_H_
+#define ATANIMATIONKIT_H_
+
+#include <ATAnimationKit/ATEasingFunctions.h>
+
+#if defined(__cplusplus)
+# include <ATAnimationKit/ATInterpolator.hpp>
+# include <ATAnimationKit/ATInterpolator+CoreGraphics.hpp>
+#endif
+
+#import <ATAnimationKit/ATDisplayLink.h>
+
+#endif /* ATANIMATIONKIT_H_ */
