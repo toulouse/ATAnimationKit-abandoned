11d77276cdae44520aa6e11b512865dd60900930
Andrew Toulouse <andrew@atoulou.se>
2014-01-04 21:32:42 -0600

    [ATAnimationKit][Example] Add looping example

--- ATAnimationKitExample/ATAnimationKitExample/ATAppDelegate.m
+++ ATAnimationKitExample/ATAnimationKitExample/ATAppDelegate.m
@@ -12,11 +12,20 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+typedef NS_ENUM(NSUInteger, ATCorner) {
+    ATCornerTopLeft,
+    ATCornerTopRight,
+    ATCornerBottomRight,
+    ATCornerBottomLeft,
+};
+
 @interface ATAppDelegate () <ATAnimationDelegate>
 
 @end
 
-@implementation ATAppDelegate
+@implementation ATAppDelegate {
+    CALayer *_cornerLoopLayer;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -25,29 +34,17 @@
 
     [[ATAnimator sharedAnimator] start];
 
-    // Add layer (any KVC-compliant Objective-C property will do)
     NSView *contentView = self.window.contentView;
     contentView.wantsLayer = YES;
 
-    CALayer *someLayer = [CALayer layer];
-    someLayer.frame = CGRectMake(0, 0, 30, 30);
-    someLayer.backgroundColor = [NSColor blueColor].CGColor;
-
-    [contentView.layer addSublayer:someLayer];
-
-    // Calculate a final rect that's at the top right corner
-    CGRect windowRect = contentView.layer.bounds;
-    CGFloat width = CGRectGetWidth(windowRect);
-    CGFloat height = CGRectGetHeight(windowRect);
-    CGRect toRect = CGRectMake(width - 150, height - 150, 150, 150);
+    _cornerLoopLayer = [CALayer layer];
+    _cornerLoopLayer.frame = CGRectMake(0, 0, 30, 30);
+    _cornerLoopLayer.backgroundColor = [NSColor blueColor].CGColor;
+    [contentView.layer addSublayer:_cornerLoopLayer];
 
     // Animate
-    ATBasicAnimation *animation = [ATBasicAnimation animationWithObject:someLayer keyPath:@"frame"];
-    animation.toValue = [NSValue valueWithCGRect:toRect];
-    animation.duration = 2;
-    animation.delegate = self;
-
-    [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"someIdentifier"];
+    ATBasicAnimation *animation = [self animationToCorner:ATCornerTopLeft];
+    [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"toTopLeft"];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification
@@ -63,6 +60,54 @@
 - (void)animationDidStop:(ATAnimation *)animation finished:(BOOL)finished
 {
     ATLogDebug(@"Stopped: %@ finished: %d", animation, finished);
+
+    if (!finished) {
+        return;
+    }
+
+    if (animation == [[ATAnimator sharedAnimator] animationForKey:@"toTopLeft"]) {
+        ATBasicAnimation *animation = [self animationToCorner:ATCornerTopRight];
+        [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"toTopRight"];
+    } else if (animation == [[ATAnimator sharedAnimator] animationForKey:@"toTopRight"]) {
+        ATBasicAnimation *animation = [self animationToCorner:ATCornerBottomRight];
+        [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"toBottomRight"];
+    } else if (animation == [[ATAnimator sharedAnimator] animationForKey:@"toBottomRight"]) {
+        ATBasicAnimation *animation = [self animationToCorner:ATCornerBottomLeft];
+        [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"toBottomLeft"];
+    } else if (animation == [[ATAnimator sharedAnimator] animationForKey:@"toBottomLeft"]) {
+        ATBasicAnimation *animation = [self animationToCorner:ATCornerTopLeft];
+        [[ATAnimator sharedAnimator] addAnimation:animation forKey:@"toTopLeft"];
+    }
+}
+
+- (ATBasicAnimation *)animationToCorner:(ATCorner)corner
+{
+    CGRect rect = ((NSView *)self.window.contentView).layer.bounds;
+    CGFloat width = CGRectGetWidth(rect);
+    CGFloat height = CGRectGetHeight(rect);
+
+    ATBasicAnimation *animation = [ATBasicAnimation animationWithObject:_cornerLoopLayer keyPath:@"frame"];
+
+    CGRect toRect;
+    switch (corner) {
+        case ATCornerTopLeft:
+            toRect = CGRectMake(0, height - 60, 60, 60);
+            break;
+        case ATCornerTopRight:
+            toRect = CGRectMake(width - 90, height - 90, 90, 90);
+            break;
+        case ATCornerBottomRight:
+            toRect = CGRectMake(width - 120, 0, 120, 120);
+            break;
+        case ATCornerBottomLeft:
+            toRect = CGRectMake(0, 0, 30, 30);
+            break;
+    }
+
+    animation.toValue = [NSValue valueWithCGRect:toRect];
+    animation.duration = 2;
+    animation.delegate = self;
+    return animation;
 }
 
 @end
